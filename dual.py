37aed7b6a620f114060b27674d1c7fe11b55003c
MP014 <165722127+MP014@users.noreply.github.com>
2024-04-23 21:02:57 +0000

    I believe in the dream

---
+++ dual.py
@@ -0,0 +1,10 @@
+print("Greetings brave adventurer! Please state your name.")
+Name=input()
+print ("Well met "+Name+"!")
+attribute= input()
+attribute=int(attribute)
+if attribute==1:
+     print("good job micah")
+else:
+     if attribute==2:
+          print("even better job micah")
