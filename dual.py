1ed796813f7235a36af81efbc5c44e8c7a5f9943
MP014 <165722127+MP014@users.noreply.github.com>
2024-04-24 03:28:01 +0000

    making the example if then actually on task

--- dual.py
+++ dual.py
@@ -1,10 +1,15 @@
 print("Greetings brave adventurer! Please state your name.")
 Name=input()
 print ("Well met "+Name+"!")
+print("pick a number between 1 and 4")
+print("if you wish to major in IT pick 1")
+print("If you wish to major in science pick 2")
+print("if you wish to major in Engineering pick 3")
+print("If you wish to major in Health Sciences pick 4")
 attribute= input()
 attribute=int(attribute)
 if attribute==1:
-     print("good job micah")
+     print("Welcome Cyborg!")
 else:
      if attribute==2:
-          print("even better job micah")
+          print("Welcome Incredible Hulk!")
